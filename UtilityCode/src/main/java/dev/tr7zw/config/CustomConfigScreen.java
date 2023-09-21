97ecea88c3b694f59ba066fcda981887f705cc02
tr7zw <tr7zw@live.de>
2023-09-21 20:54:09 +0200

    Update CustomConfigScreen.java

--- UtilityCode/src/main/java/dev/tr7zw/config/CustomConfigScreen.java
+++ UtilityCode/src/main/java/dev/tr7zw/config/CustomConfigScreen.java
@@ -83,10 +83,9 @@ public abstract class CustomConfigScreen extends Screen {
     }
 
     public void render(GuiGraphics guiGraphics, int i, int j, float f) {
-        this.renderDirtBackground(guiGraphics);
+        super.render(guiGraphics, i, j, f);
         this.list.render(guiGraphics, i, j, f);
         guiGraphics.drawCenteredString(this.font, this.title, this.width / 2, 20, 16777215);
-        super.render(guiGraphics, i, j, f);
     }
 
     private <T> TooltipSupplier<T>  getOptionalTooltip(String translationKey) {
